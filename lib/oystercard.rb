dcf838f6a8c9792f547bd8425765d51f702d2c94
russellvaughan <russellvaughan@gmail.com>
2016-01-25 20:59:16 +0000

    max balance limit and constant added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,26 @@
 class Oystercard
 
 attr_reader :balance
-#DEFAULT_VALUE = 0
+MAX_LIMIT = 90
 
   def initialize
   @balance = 0
   end
 
-  def top_up(amount)
-    @balance += amount
+  def top_up(amount)	
+  fail "Maximum limit of #{Oystercard::MAX_LIMIT} reached" if balance_exceeded(amount)
+  @balance += amount
 
   end
 
+private
+
+	def balance_exceeded(amount)
+
+	(@balance + amount) > MAX_LIMIT
+
+	end	
 
 end
+
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard) {described_class.new}
-
+ 
   	it "instaniates an oystercard class" do
   		expect(subject).to be_a Oystercard
   	end
@@ -13,12 +13,20 @@ describe Oystercard do
 
     it 'it should respond to top_up ' do
       expect(subject).to respond_to(:top_up).with(1).argument
-    end
+    end    
 
 describe '#top_up' do
 
     it 'should increase the balance by 10 when passed 10' do
       expect{subject.top_up 10}.to change{ subject.balance}.by 10
     end
-  end
+    end
+
+  context 'When maximum limit exceed' do
+    it 'should raise an error ' do
+    oystercard.top_up(Oystercard::MAX_LIMIT)
+    message = "Maximum limit of #{Oystercard::MAX_LIMIT} reached"
+    expect{oystercard.top_up 1}.to raise_error message
+    end 
+    end
 end
