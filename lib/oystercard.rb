24d631e65cb72934770ff0b454857dfcdea9d814
Finble <elainefinn@hotmail.com>
2016-01-26 15:25:22 +0000

    refactored tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,17 @@
 class Card 
 
-DEFAULT_LIMIT = 90
+MAXIMUM_LIMIT = 90
 
 	attr_reader :balance, :limit
 
-	def initialize(limit=DEFAULT_LIMIT)
+	def initialize(limit=MAXIMUM_LIMIT)
 		@balance = 0
 		@limit = limit
 	end
 
 	def top_up(amount)
 		new_balance = @balance + amount
-		message = "You cannot exceed the #{@limit} limit!"
+		message = "You cannot exceed the £#{@limit} limit!"
 		raise (message) if new_balance > @limit
 		@balance += amount
 	end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,31 @@
 require 'oystercard'
 
-describe Card  do 
+RSpec.describe Card do 
 
-	   limit = Card::DEFAULT_LIMIT
+limit = Card::MAXIMUM_LIMIT
+
+	it 'creates a new card' do
+		expect(Card.new).to be_a(Card)
+	end
+
+	context 'balance' do
 
 		it 'has a balance of zero' do
 		expect(subject.balance).to eq(0)
 		end 
+	end
+
+	context 'top ups' do
 
 		it {is_expected.to respond_to(:top_up).with(1).argument }
 
 		it 'tops up the balance' do
 		expect{ subject.top_up(1) }.to change{ subject.balance }.by(1)
 		end
+
+	end
+
+	context 'limit' do
 
 		it {is_expected.to respond_to(:limit)} 
 
@@ -21,13 +34,19 @@ describe Card  do
 		end
 
 		it 'has a default limit' do
-		expect(subject.limit).to eq limit # Card::DEFAULT_LIMIT
+		expect(subject.limit).to eq limit # Card::MAXIMUM_LIMIT
 		end
 
 		it "cannot exceed #{limit}" do
-		message = "You cannot exceed the #{limit} limit!"
+		message = "You cannot exceed the £#{limit} limit!"
 		subject.top_up(limit)  # the value needs to be less than the limit less what is topped up in test (so if default value = 90, and you top up 1 or more, you need value of 89 or less)
 		expect{subject.top_up(1)}.to raise_error(message)
 		end
-
+	end
 end
+
+
+
+
+
+
