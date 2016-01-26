df4683ee1b38935782bc6cf819b1173a4230e854
Frazer Watson <frazerw@tutanota.com>
2016-01-26 15:50:46 +0000

    Challenge 4 Enforce Maximum Balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,22 @@
 class OysterCard
 
+ MAXIMUM_BALANCE = 90
+
  attr_reader :balance	
 
  def initialize
    @balance = 0
  end
 
- def add_balance(amount)
+ def top_up(amount)
+ 	raise "error balance greater than #{MAXIMUM_BALANCE}" if balance + amount > MAXIMUM_BALANCE
  	@balance += amount
  end
 
+
+
+
+
+
 end
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,9 +5,20 @@ describe OysterCard do
   	expect(subject.balance).to eq(0)
   end
 
-describe '#add_balance' do
+describe '#top_up' do
   it 'adds amount to balance' do
-  expect{subject.add_balance(1)}.to change{subject.balance}.by 1
+  expect{subject.top_up(1)}.to change{subject.balance}.by 1
  end 
 end
+
+it 'raises an error when balance more than 90' do
+    maximum_balance = OysterCard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+	expect{ subject.top_up(1) }.to raise_error "error balance greater than #{maximum_balance}"
+end
+
+
+
+
+
 end
