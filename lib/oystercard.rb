861fe7ddd52750c3bafdb594db939bf52ac5ddca
russellvaughan <russellvaughan@gmail.com>
2016-01-27 16:58:16 +0000

    create Station Class as a Struct.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,4 @@
-
+require './lib/station'
 
 class OysterCard
 
---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require "./lib/station.rb"
+                                          
+describe Station do
+
+  subject {described_class.new("Old Street", 1)}
+
+  it 'knows its name' do                      
+    expect(subject.name).to eq("Old Street")              
+  end                                                   
+
+  it 'knows its zone' do                                                     
+    expect(subject.zone).to eq(1)                                 
+  end
+end
